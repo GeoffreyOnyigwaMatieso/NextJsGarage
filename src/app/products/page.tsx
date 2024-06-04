ce0ec520b1726e4919e5e7bf99d454d83e05a0b6
Tech_G <matiesogeoffrey@gmail.com>
2024-06-04 12:44:05 +0300

    Dynamic routing Done

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductsList() {
+    return(
+        <>
+            <h1>Products Page</h1>
+            <h2>Product 1</h2>
+            <h3>Products 2</h3>
+            <h4>Product 3</h4>
+        </>
+    
+)
+}
