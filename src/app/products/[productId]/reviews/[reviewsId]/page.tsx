5f954ed5979616172e9eb31e969a7512c8914886
Tech_G <matiesogeoffrey@gmail.com>
2024-06-04 13:07:23 +0300

    Nested Dynamic Routes Done

---
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -0,0 +1,12 @@
+export default function ReviewDetail({params}: {
+    params: {
+        productId: string;
+        reviewsId: string;
+    }
+}){
+    return (
+        <>
+            <h1>Review {params.reviewsId} for product {params.productId}</h1>
+        </>
+    )
+}
